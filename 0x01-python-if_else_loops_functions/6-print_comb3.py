0ff3cc305baf25f6727d2083fcdb459b537cc8f7
Geekbooi <yosephmekonnen2@gmail.com>
2022-05-31 14:07:24 -0700

    com

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i + 1, 10):
-        print(
-            "{}{}".format(i, j),
-            end=", " if int(str(i) + str(j)) < 89 else "\n"
-            )
+    for j in range(10):
+        if i < j:
+            print('{:d}{:d}'.format(i, j), end='')
+            if i < 8:
+                print(', ', end='')
+print()
