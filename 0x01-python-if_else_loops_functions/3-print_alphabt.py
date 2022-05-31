f51d8cc424bc46ccff731c3ffd023a377acbd76a
Geekbooi <yosephmekonnen2@gmail.com>
2022-05-31 14:00:08 -0700

    print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in 'abcdefghijklmnopqrstuvwxyz':
-    if (i != "q" and i != "e"):
-        print("{}".format(i), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print('{:c}'.format(i), end='')
