aafda3b557a12b1a3a99ebaff9f2c7249c32b84b
Geekbooi <yosephmekonnen@gmail.com>
2022-06-09 23:37:42 +0300

    iterate over a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    print(simple_delete)
     if key in a_dictionary:
         del a_dictionary[key]
-    return a_dictionary
+        return a_dictionary
+
